78a3c20be8b9e4bb139dae0f770f81e7736d48c7
bsc-com-19-20 <bsc-com-19-20@unima.ac.mw>
2023-04-17 10:55:45 +0200

    project setup

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+  .setTitle('E-Shopping')
+  .setDescription('Site where you can order stuff online')
+  .setVersion('1.0')
+  .addTag('E-Shopping')
+  .build();
+const document = SwaggerModule.createDocument(app, config);
+SwaggerModule.setup('api', app, document);
+
+  await app.listen(3002);
+}
+bootstrap();
